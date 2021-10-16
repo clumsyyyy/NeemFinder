f1a0c22de3eb31f221e4dd3606e81437a62cbf61
clumsyyyy <owencwijaya@gmail.com>
2021-10-16 19:28:10 +0700

    tambahin handler buat kasus masih nim TPB

--- src/components/displayData.js
+++ src/components/displayData.js
@@ -1,7 +1,7 @@
 var tempData = [
     ["Mahasiswa 1", "16520001", "13520001"],
     ["Mahasiswa 2", "16520002", "13520002"],
-    ["Mahasiswa 3", "16520003", "13520003"]
+    ["Mahasiswa 3", "16520003"]
 ];
 
 localStorage.setItem("tempData", JSON.stringify(tempData));
@@ -19,13 +19,19 @@ export function generateContainer(){
     
         const NIM_TPB = document.createElement("p");
         NIM_TPB.innerText = tempData[i][1];
-        
-        const NIM_Jurusan = document.createElement("p");
-        NIM_Jurusan.innerText = tempData[i][2];
+
+        var NIM_Jurusan = document.createElement("p");;
+        NIM_Jurusan.innerText = "-";
+
+        if (tempData[i].length === 3){
+            NIM_Jurusan.innerText = tempData[i][2];
+        }
 
         const NIMContainer = document.createElement("div");
         NIMContainer.append(NIM_TPB, NIM_Jurusan);
+        
         NIMContainer.classList.add("NIMContainer");
+        
 
         const mahasiswaContainer = document.createElement("div");
         mahasiswaContainer.append(nama, NIMContainer);
