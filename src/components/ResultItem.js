16948977c5e5e8f269121c6637bd8bad32a97053
clumsyyyy <71161031+clumsyyyy@users.noreply.github.com>
2021-10-17 14:16:46 +0700

    Menambahkan div untuk nama + info

--- src/components/ResultItem.js
+++ src/components/ResultItem.js
@@ -1,10 +1,14 @@
 import React from "react";
 
-export default function ResultItem({ nim_tpb, nim_jurusan, nama }) {
+export default function ResultItem({ nim_tpb, nim_jurusan, nama, info }) {
   return (
     <div className="container">
       <div>
-        <h2>{nama}</h2>
+        <div className="NIContainer">
+          <h2>{nama}</h2>
+          <p>{info}</p>
+        </div>
+  
         <div className="NIMContainer">
           <p>{nim_tpb}</p>
           {nim_jurusan && <p>{nim_jurusan}</p>}
