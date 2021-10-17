4ce2482705ecf9c8b1eca179fc246904c8f12b70
clumsyyyy <71161031+clumsyyyy@users.noreply.github.com>
2021-10-17 14:18:15 +0700

    Mengubah nama fetchData

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import "./Results.css";
-import "./loadData.js";
-import "./components/displayData.js";
+import "./components/fetchData.js";
 import Results from "./components/Results";
 import Search from "./components/Search";
 import { useLoadData } from "./hooks/LoadData";
