fc7c5e037369d03a1810a0614adde3dd6e84cce6
clumsyyyy <owencwijaya@gmail.com>
2021-10-17 14:06:45 +0700

    Penambahan algoritma untuk keterangan jurusan (dummy data)

--- src/hooks/LoadData.js
+++ src/hooks/LoadData.js
@@ -21,12 +21,30 @@ export function useLoadLocalData() {
 
 export async function downloadDatabase() {
   // Dummy data
-
-  return [
+  var tempData = [
     ["Mahasiswa 1", "16520001", "13520001"],
     ["Mahasiswa 2", "16520002", "13520002"],
-    ["Mahasiswa 3", "16520003"],
-  ];
+    ["Mahasiswa 3", "16520003"]]
+  var i = 0;
+
+  for(i = 0; i < tempData.length; i++){
+    var kode = "";
+    var data = ""
+    
+    if (tempData[i].length === 2){
+      tempData[i].push("-");
+      kode = tempData[i][1].substr(0, 3);
+      data = "TPB "+ require("../components/data/kodeTPB.json")[parseInt(kode)];
+
+    } else if (tempData[i].length === 3){
+      kode = tempData[i][2].substr(0, 3);
+      data = require("../components/data/kodeJurusan.json")[parseInt(kode)];
+    }
+
+    tempData[i].push(data);
+  }
+
+  return tempData;
 }
 
 export function useLoadData() {
