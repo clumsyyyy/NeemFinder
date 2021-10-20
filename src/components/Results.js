5c1b1f99ab488ef90ee015a6ac5674703f315178
clumsyyyy <owencwijaya@gmail.com>
2021-10-20 13:09:03 +0700

    Menambahkan properti hidden

--- src/components/Results.js
+++ src/components/Results.js
@@ -4,17 +4,27 @@ import { MahasiswaContext } from "./MahasiswaProvider";
 
 export default function ResultsComponent() {
   const { mahasiswa } = useContext(MahasiswaContext);
-  return (
-    <div className="results" id="results">
-      {mahasiswa.map(([nama, nim_tpb, nim_jurusan, info]) => (
-        <ResultItem
-          key={nim_tpb}
-          nama={nama}
-          nim_tpb={nim_tpb}
-          nim_jurusan={nim_jurusan}
-          info = {info}
-        ></ResultItem>
-      ))}
-    </div>
-  );
+  if (mahasiswa.length > 0){
+    return (
+      <div className="results" id="results" hidden>
+        <h3>{mahasiswa.length} hasil</h3>
+        {mahasiswa.map(([nama, nim_tpb, nim_jurusan, info]) => (
+          <ResultItem
+            key={nim_tpb}
+            nama={nama}
+            nim_tpb={nim_tpb}
+            nim_jurusan={nim_jurusan}
+            info = {info}
+          ></ResultItem>
+        ))}
+      </div>
+    );
+  } else {
+    return (
+      <div className="results" id="results" hidden>
+        <h2>Hasil tidak ditemukan :(</h2>
+      </div>  
+    );
+  }
+  
 }
