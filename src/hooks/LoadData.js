c3fc1979e715e58f1f1f349d8aeaaee0f7fba89c
clumsyyyy <owencwijaya@gmail.com>
2021-10-20 13:09:53 +0700

    Menyesuaikan algoritma pencocokan NIM dengan jurusan

--- src/hooks/LoadData.js
+++ src/hooks/LoadData.js
@@ -19,45 +19,42 @@ export function useLoadLocalData() {
   };
 }
 
-export async function downloadDatabase() {
+export async function DownloadDatabase() {
   // Dummy data
-  var tempData = [
-    ["Mahasiswa 1", "16520001", "13520001"],
-    ["Mahasiswa 2", "16520002", "13520002"],
-    ["Mahasiswa 3", "16520003"]]
-  var i = 0;
-
-  for(i = 0; i < tempData.length; i++){
-    var kode = "";
-    var data = ""
-    
-    if (tempData[i].length === 2){
-      tempData[i].push("-");
-      kode = tempData[i][1].substr(0, 3);
-      data = "TPB "+ require("../components/data/kodeTPB.json")[parseInt(kode)];
-
-    } else if (tempData[i].length === 3){
-      kode = tempData[i][2].substr(0, 3);
-      data = require("../components/data/kodeJurusan.json")[parseInt(kode)];
+    var tempData = require("../components/data/data.json");
+    var i = 0;
+    for(i = 0; i < tempData.length; i++){
+      var kode = "";
+      var data = "";
+      if (tempData[i].length === 2){
+        tempData[i].push("-");
+        kode = tempData[i][1].substr(0, 3);
+        data = "TPB "+ require("../components/data/kodeTPB.json")[parseInt(kode)];
+  
+      } else if (tempData[i].length === 3){
+        kode = tempData[i][2].substr(0, 3);
+        data = require("../components/data/kodeJurusan.json")[parseInt(kode)];
+      }
+  
+      tempData[i].push(data);
     }
-
-    tempData[i].push(data);
-  }
-
-  return tempData;
+  
+    return tempData;
+  
 }
 
 export function useLoadData() {
   const { setDatabase } = useContext(MahasiswaContext);
+
   const localData = useLoadLocalData();
 
   return async () => {
     if (!localData()) {
-      const data = await downloadDatabase();
+      const data = await DownloadDatabase();
       localStorage.setItem("database", JSON.stringify(data));
       setDatabase(data);
     }
-
+    
     return true;
   };
 }
